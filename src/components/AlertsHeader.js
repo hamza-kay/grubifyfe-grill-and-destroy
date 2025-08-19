5c56ff9a296fa2bf25b5160a4539f0682411e3bd
Hamza <hamzahk1991@gmail.com>
2025-08-19 19:31:19 +0100

    added different styles to alertsheader img

--- src/components/AlertsHeader.js
+++ src/components/AlertsHeader.js
@@ -63,13 +63,14 @@ return (
 
       {/* Left Image */}
       <div className="md:flex-shrink-0 w-full md:w-64 h-64 relative">
-        <Image
-          src={restaurant.image || "/images/placeholder.jpg"}
-          alt={restaurant.title}
-          
-          className="object-cover"
-          priority
-        />
+     <Image
+  src={restaurant.image || "/images/placeholder.jpg"}
+  alt={restaurant.title}
+  width={256}
+  height={256}
+  className="object-cover w-full h-full"
+  priority
+/>
       </div>
 
       {/* Right Content */}
