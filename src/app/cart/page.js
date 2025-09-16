656c9356fa52c04ef26602ad8aa81e8f71e4c5a9
Hamza <hamzahk1991@gmail.com>
2025-09-16 13:13:58 +0100

    removed min-h 800 from cart page that was causing a gap

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -7,8 +7,8 @@ export default function CartPage() {
   return (
     <main className="max-w-7xl mx-auto px-4 py-10">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
-        {/* Left: Checkout Form */}
-        <div className="lg:col-span-2 min-h-[800px] transition-all">
+        {/* Left: Checkout Form removed min-h-[800px] */}
+        <div className="lg:col-span-2  transition-all">
           <CartForm />
         </div>
 
